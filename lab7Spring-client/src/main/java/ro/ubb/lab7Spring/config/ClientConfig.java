badf52f4e93eecd300f3ac0c0a9ca336d4cfbf99
Paula Risteiu <paula.risteiu@gmail.com>
2025-02-03 14:49:00 +0200

    gitignore and architecture

---
+++ lab7Spring-client/src/main/java/ro/ubb/lab7Spring/config/ClientConfig.java
@@ -0,0 +1,4 @@
+package ro.ubb.lab7Spring.config;
+
+public class ClientConfig {
+}
---
+++ lab7Spring-web/src/main/java/ro/ubb/lab7Spring/Main.java
@@ -0,0 +1,7 @@
+package ro.ubb.lab7Spring;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
